73fc7cb8586b03dcfdf597627c2bf91be988e6aa
bpnpdl <bipinpaudel6774@gmail.com>
2023-04-17 08:02:41 +0545

    Cookie program solved

--- Servlet Programs/src/java/cookiedemo/SecondServlet.java
+++ Servlet Programs/src/java/cookiedemo/SecondServlet.java
@@ -8,6 +8,25 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class SecondServlet {
+public class SecondServlet extends HttpServlet{
+    public void doPost(HttpServletRequest request,HttpServletResponse  response) throws ServletException,IOException{
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        
+        
+       Cookie ck[]=request.getCookies();
+   
+
+            out.println("\n Welcome "+ck[0].getValue());
+            
+       
+      
+       
+       out.close();
+        
+       
+        
+    }
     
 }
+ 
